53d6e76e63474195eb513a85566d8e280c318f41
is <stupin.il@yandex.ru>
2025-03-12 18:18:51 +0300

    Пустой проект

---
+++ YourWheel.Domain/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YourWheel.Domain.Models;
+
+public partial class Client
+{
+    public Guid Clientid { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
